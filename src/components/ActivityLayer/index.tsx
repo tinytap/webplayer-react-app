5cfb01030618de45a23c1b957fc8154d0db309aa
AvivTiny <aviv@tinytap.com>
2023-06-20 10:41:51 +0300

    organasie files

--- src/components/ActivityLayer/index.tsx
+++ src/components/ActivityLayer/index.tsx
@@ -1,9 +1,7 @@
-import { useEffect } from 'react'
-import { Rect, Star } from 'react-konva'
-import { Activity, ActivitySettings, ActivityState } from '../../stores/activitiesStoreTypes'
-import useSound from 'use-sound'
+import { Star } from 'react-konva'
+import { Activity, ActivityState } from '../../stores/activitiesStoreTypes'
 import { useGameStore } from '../../stores/gameStore'
-import { PLAYER_HEIGHT, PLAYER_WIDTH } from '../../utils/constants'
+import { ReadingActivity } from './ReadingActivity'
 
 interface ActivityLayerProps {
   baseUrl: string
@@ -50,47 +48,5 @@ export function ActivityLayer({ baseUrl, activity, activityState, engine }: Acti
     default:
       return <Star numPoints={10} innerRadius={50} outerRadius={100} x={300} y={300} fill="red" />
   }
-}
-
-function ReadingActivity({
-  moveToNextSlide,
-  soundUrl,
-  activityState,
-  transitionLoading,
-  activitySettings,
-}: {
-  moveToNextSlide: (index: number | undefined) => void
-  soundUrl: string
-  activityState: ActivityState
-  transitionLoading: boolean
-  activitySettings: ActivitySettings
-}) {
-  const [play, { stop }] = useSound(soundUrl, {
-    onend: () => {
-      if (!activitySettings.advance || activityState.doesSlideHaveClickableLayer) {
-        return
-      }
-      moveToNextSlide(activitySettings.linkToPage)
-    },
-  })
-
-  const onClick = () => {
-    if (activitySettings.advance || activityState.doesSlideHaveClickableLayer) {
-      return
-    }
-    stop()
-    moveToNextSlide(activitySettings.linkToPage)
-  }
-
-  useEffect(() => {
-    if (!activityState.started || activityState.paused || transitionLoading) {
-      stop()
-      return
-    }
-
-    play()
-  }, [activityState, play, transitionLoading])
-
-  return <Rect x={0} y={0} width={PLAYER_WIDTH} height={PLAYER_HEIGHT} onClick={onClick} />
 }
 
